a72b7488fd4fab67783c370e5ad89f4c3839918b
Yuniru Yuni <yuniruyuni@gmail.com>
2022-08-21 14:32:27 +0900

    define internal/graphical position

--- checkers/src/pos.rs
+++ checkers/src/pos.rs
@@ -23,6 +23,26 @@ impl Pos {
         let ix = (7-x) / 2;
         Some(Pos::new(ix, iy))
     }
+
+    /// x returns internal position-x for this Pos.
+    pub fn x(self) -> u8 {
+        self.0 & 0x3
+    }
+
+    /// y returns internal position-x for this Pos.
+    pub fn y(self) -> u8 {
+        self.0 >> 2 // & 0x3 // We can assume self.0 is less than 0x1F for pre-condition.
+    }
+
+    /// gx returns graphical position-x for this Pos.
+    pub fn gx(self) -> u8 {
+        7 - ((self.x() << 1) + (1-(self.y() % 2)))
+    }
+
+    /// gy returns graphical position-y for this Pos.
+    pub fn gy(self) -> u8 {
+        7 - self.y()
+    }
 }
 
 
@@ -32,7 +52,7 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn graphical_expression_converted_correctly() {
+    fn convert_graphical_position() {
         let cases = [
             ((0, 0), None, "Left-Top"),
             ((1, 0), Some(31), "Right side from Left-Top"),
@@ -55,5 +75,41 @@ mod tests {
             let actual = Pos::graphical(x, y);
             assert_eq!(expect, actual, "{}", msg);
         }
+    }
+
+    #[test]
+    fn can_read_x() {
+        let expect = 3;
+        let actual = Pos::new(3, 2).x();
+        assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn can_read_y() {
+        let expect = 3;
+        let actual = Pos::new(2, 3).y();
+        assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn can_read_gx() {
+        {
+            let expect = 4;
+            let actual = Pos::graphical(4, 3).unwrap().gx();
+            assert_eq!(expect, actual, "the line started from active cell");
+        }
+
+        {
+            let expect = 3;
+            let actual = Pos::graphical(3, 2).unwrap().gx();
+            assert_eq!(expect, actual, "the line started from inactive cell");
+        }
+    }
+
+    #[test]
+    fn can_read_gy() {
+        let expect = 3;
+        let actual = Pos::graphical(4, 3).unwrap().gy();
+        assert_eq!(expect, actual);
     }
 }
