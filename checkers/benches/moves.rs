4ca5e4e497fb45d830e39566f6c6f4933ba6db93
yuniruyuni <yuniruyuni@gmail.com>
2022-09-11 10:57:22 +0900

    bugfix: compile error for benchmarks

--- checkers/benches/moves.rs
+++ checkers/benches/moves.rs
@@ -5,6 +5,7 @@ fn moves(c: &mut Criterion) {
         b.iter(move || {
             let game = checkers::Game{
                 side: checkers::Player::BLK,
+                jumping: None,
                 blk: checkers::Board::new(0b0000_0000_0000_0000_0000_1111_1111_1111),
                 red: checkers::Board::new(0b1111_1111_1111_0000_0000_0000_0000_0000),
                 king: checkers::Board::new(0b0000_0000_0000_0000_0000_0000_0000_0000),
@@ -18,6 +19,7 @@ fn moves(c: &mut Criterion) {
         b.iter(move || {
             let game = checkers::Game{
                 side: checkers::Player::BLK,
+                jumping: None,
                 blk: checkers::Board::new(0b0000_0010_0000_0100_0000_0000_1111_0000),
                 red: checkers::Board::new(0b1010_0000_0101_0000_1010_0000_0000_0000),
                 king: checkers::Board::new(0b0010_0000_0001_0000_0000_0000_0010_0000),
